c29c8d5ebefa6df081212d997fd10a03a100e3d8
Diane Tuyizere <dtuyizere@smith.edu>
2025-09-17 11:18:59 -0400

    Added Some Atrributes, contructor and Methods

--- computer.py
+++ computer.py
@@ -1,10 +1,47 @@
 class Computer:
 
-    # What attributes will it need?
+    # What attributes will it need? This will need Brand,Model, Os, Display, Memory, storage, Battery, Year of Manufcturing and Price
+    # computer attributes
+    str Brand=""
+    str Model=""
+    str Os=""
+    str Display=""
+    str Memory=""
+    str Storage=""
+    str Battery=""
+    int Year_of_Manufacturing=0
+    float Price=0.0
+
 
     # How will you set up your constructor?
+    # Constructor: it should have the computer attributes not as parameters
+    def __init__():
+        self.Branbd=Brand
+        self.Model=Model
+        self.Os=Os
+        self.Display=Display
+        self.Memory=Memory
+        self.Storage=Storage
+        self.Battery=Battery
+        self.Year_of_Manufacturing=Year_of_Manufacturing
+        self.Price=Price
+
+
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    # For the method I choose to have them into 3 main Partitions.
+    #1. Brand-Model-Os
+    #2. Display-Memory-Storage
+    #3. Battery-Year of Manufacturing-Price
+    def BrandModelOs():
+         # Not sure what to put here yet
+         pass
+    def DisplayMemoryStorage():
+         # Not sure what to put here yet
+         pass
+    def BatteryYearOfManufacturingPrice():
+         # Not sure what to put here yet
+         pass
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,31 @@
+import computer.py
 class ResaleShop:
 
     # What attributes will it need?
+    # I think attributes here will be mostly what the shop has
+    str ComputerBrands="" # I think each brand shall also have all the necessary computer attribures in the store.
+    int BrandCounts=0
+
 
     # How will you set up your constructor?
+    def __init__():
+        self.ComputerBrands=ComputerBrands
+        self.BrandCounts=BrandCounts
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    # I think methods here will be mostly about updating the inventory based on the computer information
+    def UpdateInventory():
+         # Not sure what to put here yet
+         pass
+    def CheckinComputer():
+
+
+         # Not sure what to put here yet
+         pass
+    def CheckoutComputer():
+         # Not sure what to put here yet
+         pass
+   
