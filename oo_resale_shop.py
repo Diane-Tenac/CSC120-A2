4df8aac4c59863994ec09645c1f46213e6f6a6c2
Diane Tuyizere <dtuyizere@smith.edu>
2025-09-18 16:07:45 -0400

    Just the comments about what needs to be brainstormed about---Using boolean in controlling Checkins and outs.

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -18,16 +18,20 @@ class ResaleShop:
     # What methods will you need?
     # I think methods here will be mostly about updating the inventory based on the computer information
     def UpdateInventory():
-         # Not sure what to put here yet
-         pass
+        # Not sure what to put here yet
+        pass
     def CheckinComputer(Brand, Model, Os, Display, Memory, Storage, Battery, Year_of_Manufacturing, Price ):
-         # thinking of updating counts +1 whaen a computer is checked in but in a way that it will be recording information based on brands
-         
+        # thinking of updating counts +1 whaen a computer is checked in but in a way that it will be recording information based on brands
+        # I have got an idea of having something like a list/dictionary that can store computer information and use conditions to recors them based on the brands/other attributes like OS, Model etc.
+        # To Keep it simple for now, I will just update the counts +1
+        
+        self.BrandCounts+=1
 
-
-         # Not sure what to put here yet
-         pass
+        # Not sure what to put here yet
+        pass
     def CheckoutComputer():
-         # Not sure what to put here yet
-         pass
+        # Not sure what to put here yet
+        self.BrandCounts-=1
+        pass
+    #### I want to use boolean for checking if there are transactions made like checkin/checkout, so I can use that to update the inventory but I am stuck in ideas.
    
